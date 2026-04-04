6c1353cc7a9b8b5ceace3a84578887f7e10d6eed
florianZac <florianaizac@msn.com>
2026-04-04 02:59:08 +0200

    Mise en place des mentions légales

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,14 +10,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(
-        LocalisationRepository $localisationRepository,
-        TemoignageRepository $temoignageRepository
-    ): Response {
-        return $this->render('home/index.html.twig', [
-            'localisations' => $localisationRepository->findAllWithAppartements(),
-            'temoignages' => $temoignageRepository->findActifs(),
-        ]);
-    }
+  #[Route('/', name: 'app_home')]
+  public function index(
+    LocalisationRepository $localisationRepository,
+    TemoignageRepository $temoignageRepository
+  ): Response {
+    return $this->render('home/index.html.twig', [
+      'localisations' => $localisationRepository->findAllWithAppartements(),
+      'temoignages' => $temoignageRepository->findActifs(),
+    ]);
+  }
+
+  #[Route('/mentions-legales', name: 'app_mentions_legales')]
+  public function mentionsLegales(): Response
+  {
+    return $this->render('home/mentions_legales.html.twig');
+  }
 }
